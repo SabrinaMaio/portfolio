45acebc9245adc019c5fc1d2b5b6f36b01e08763
SabrinaMaio <sabrinamaioribeiro@gmail.com>
2023-12-22 14:00:53 +0100

    Initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {link} from "svelte-spa-router"
+</script>
+
+<header>
+    <nav>
+        <ul>
+            <li><a use:link href="/">Accueil</a></li>
+            <li><a use:link href="/portfolio">Portfolio</a></li>
+            <li><a use:link href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<h2>Coucou</h2>
+
+
+<style lang="scss">
+
+    h2 {color: orange;}
+
+    header {
+        color: black;
+
+        li {background: lightgray}
+    }
+
+</style>
